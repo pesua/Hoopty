ccd36ce79e0936e2f30d11576cde6b9dedd716c8
taran-alex91 <taran.alex91@gmail.com>
2018-05-23 00:16:31 +0300

    add line_race.ino

---
+++ line_race.ino
@@ -0,0 +1,70 @@
+const int STRAIGHT = 1;
+const int RIGHT10 = 2;
+const int RIGHT20 = 3;
+const int LEFT10 = 4;
+const int LEFT20 = 5;
+
+state = STRAIGHT;
+
+void line_race() {
+  
+  switch (state)  {
+    
+    case STRAIGHT:
+      stright();
+      
+      refreshSensor(d3);
+      if (d3 == 1)
+        state = RIGHT10;
+
+      refreshSensor(d5);
+      if (d5 == 1)
+        state = LEFT10;
+    break;
+
+    case RIGHT10:
+      left10();
+
+      refreshSensor(d2);
+      if (d2 == 1)
+        state = RIGHT20;
+
+      refreshSensor(d4);
+      if (d4 == 1)
+        state = STRAIGHT;
+    break;
+
+    case RIGHT20:
+      left20();
+
+      refreshSensor(d3);
+      if (d3 == 1)
+        state = RIGHT10;
+    break;
+
+    case LEFT10:
+      right10();
+
+      refreshSensor(d6);
+      if (d6 == 1)
+        state = LEFT20;
+
+      refreshSensor(d4);
+      if (d4 == 1)
+        state = STRAIGHT;
+    break;
+
+    case LEFT20:
+       right20();
+
+      refreshSensor(d5);
+      if (d5 == 1)
+        state = LEFT10;
+
+
+    break;
+
+    
+  }
+
+}
